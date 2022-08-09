fababd6214da03610f09012dbf30acfafa52dbd5
Frederick Hazel <frederick.d9696@gmail.com>
2022-08-09 17:47:37 -0500

    account route done

--- bank/routers/account.py
+++ bank/routers/account.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from typing import List, Optional
 from fastapi import Depends, status, Response, HTTPException
 from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from ..db import database
 from ..schemas import account
 from ..db.database import db
@@ -16,13 +17,25 @@ router = APIRouter(
 
 
 @router.post(
-    "/account",
+    "/",
     status_code=status.HTTP_201_CREATED,
 )
 async def create_account(request: account.AccountModel):
-    account = jsonable_encoder(request)
-    await db["accounts"].insert_one(account)
-    return {"message": "Account created"}
+    try:
+        account = jsonable_encoder(request)
+        await db["accounts"].insert_one(account)
+        return {"message": "Account created"}
+    except:
+        raise HTTPException(status_code=500, detail=f"Error")
+
+
+@router.get(
+    "/{id}", status_code=status.HTTP_200_OK, response_model=account.AccountModel
+)
+async def get_account(id: str):
+    if (account := await db["accounts"].find_one({"account_id": id})) is not None:
+        return account
+    raise HTTPException(status_code=404, detail=f"Account {id} not found")
 
 
 @router.get(
@@ -31,3 +44,26 @@ async def create_account(request: account.AccountModel):
 async def get_accounts():
     accounts = await db["accounts"].find().to_list(1000)
     return accounts
+
+
+@router.put(
+    "/update-account/{id}",
+    status_code=status.HTTP_202_ACCEPTED,
+)
+async def update_account(id: str, request: account.UpdateAccountModel):
+    account = jsonable_encoder(request)
+    update_result = await db["accounts"].update_one(
+        {"account_id": id}, {"$set": account}
+    )
+    if update_result.modified_count != 1:
+        raise HTTPException(status_code=404, detail=f"Account {id} not found")
+    return {"message": "Account Updated"}
+
+
+@router.delete("/{id}", status_code=status.HTTP_200_OK)
+async def delete_account(id: str):
+    delete_result = await db["accounts"].delete_one({"account_id": id})
+
+    if delete_result.deleted_count != 1:
+        raise HTTPException(status_code=404, detail=f"Student {id} not found")
+    return {"message": "Account Deleted"}
--- bank/schemas/account.py
+++ bank/schemas/account.py
@@ -10,7 +10,6 @@ class AccountModel(BaseModel):
     name: str = Field(...)
     balance: float = Field(...)
     user_id: str = Field(...)
-    pin: str = Field(...)
     account_id: str = Field(...)
 
     class Config:
@@ -22,25 +21,19 @@ class AccountModel(BaseModel):
                 "name": "Test Account",
                 "balance": 3000,
                 "user_id": "USR1",
-                "pin": "1234",
+                "account_id": "ACC2",
             }
         }
 
 
 class UpdateAccountModel(BaseModel):
-    name: Optional[str]
     balance: Optional[float]
-    course: Optional[str]
-    pin: Optional[str]
 
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
         schema_extra = {
             "example": {
-                "name": "Test Account",
                 "balance": 3000,
-                "user_id": "USR1",
-                "pin": "1234",
             }
         }
